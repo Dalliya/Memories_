ee058b0be44c37c0e6dec34e6605503e56e42f8c
Dalliya <dalliyal@gmail.com>
2022-05-21 01:36:08 +0200

    Lesson_7

---
+++ Lesson_7.py
@@ -0,0 +1,10 @@
+#Loops - циклы
+
+#for i in range(5):
+#    if i % 2 == 0:
+#        print("Hello world!", i)
+#    else:
+#        print("Happy New Year!", i)
+
+for i in range(10, 20, 2):
+    print("Iteration#: ", i)
