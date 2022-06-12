101d77525eb13f97e288495c7167b43cf309edb2
Dalliya <dalliyal@gmail.com>
2022-06-12 17:02:08 +0200

    Task_m_19

---
+++ Task_m_19.py
@@ -0,0 +1,26 @@
+# Написать функцию для поиска разницы между максимальным и минимальным числом среди num_limit случайно сгенерированных чисел в
+# указанном числовом диапазоне.
+# def diff_min_max(num_limit, lower_bound, upper_bound): # returns int
+# pass
+
+import random
+
+lower_bound = int(input('Input lower bound: '))
+upper_bound = int(input('Input upper bound: '))
+num_limit = 101
+
+def diff_min_max(num_limit, lower_bound, upper_bound):
+    for i in range(num_limit):
+        rand_num = random.randint(lower_bound, upper_bound)
+        print(rand_num)
+        lst = []
+        lst += lst.append(rand_num)
+        print(lst)
+    diff = max(lst) - min(lst)
+    return diff
+
+print(diff_min_max(num_limit, lower_bound, upper_bound))
+
+
+
+
