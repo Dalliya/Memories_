0d672236ba0e2166a1139685ecc8f009eb1148ee
Dalliya <dalliyal@gmail.com>
2022-08-09 01:29:43 +0200

    Lesson_10

--- Lesson_10.py
+++ Lesson_10.py
@@ -11,8 +11,10 @@ mult_table(9, 9)
 #Вывести на экран шахматную доску
 
 def chess_table(rows = 8):
+    print()
     for i in range(1, rows + 1):
-        for c in 'ABCDEFGH':
-            print("{}{}".format(c, i), end='\t')
+        for j in range(ord('A'), ord('H') + 1):
+#        for c in 'ABCDEFGH':
+            print("{}{}".format(chr(j), i), end='\t')
         print()
 chess_table()
