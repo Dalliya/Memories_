bf0932b5bea3101e461afaaf3543882789386e67
Dalliya <dalliyal@gmail.com>
2022-08-10 22:23:58 +0200

    Task_m_25_Pythagoras_Table

--- Task_m_25_Pythagoras_Table.py
+++ Task_m_25_Pythagoras_Table.py
@@ -3,7 +3,7 @@
 def print_mult_table(n, m): # prints multiplication table n x m
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            print("{}". format(i, j), end= '\t')
+            print("{}". format(i * j), end='\t')
         print()
-print_mult_table()
+print_mult_table(9, 9)
 
