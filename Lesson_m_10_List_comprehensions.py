8c996265e6c5e58a124ca42b8ce4c9cecf08aabd
Dalliya <dalliyal@gmail.com>
2022-08-10 21:18:25 +0200

    Lesson_10_List_Comprehensions_Recursion_Fibonacchi_Factorial

--- Lesson_m_10_List_comprehensions.py
+++ Lesson_m_10_List_comprehensions.py
@@ -52,3 +52,41 @@ Lesson_9_Nested_lists_matrix.pretty_print_matrix(matrix)
 random_matrix = [[random.randint(-5, 5) for _ in range(columns)] for _ in range(rows)]
 Lesson_9_Nested_lists_matrix.pretty_print_matrix(random_matrix)
 
+print("*****************************************************************")
+
+print(sum([1, 2, 3]))
+
+print("*****************************************************************")
+
+print(sum(i for i in range(1, 101)))
+print(min(i for i in range(1, 101)))
+print(max(i for i in range(1, 101)))
+
+print(max(random.randint(-50, 50) for _ in range(100)))
+
+print("*****************************************************************")
+
+#Рекурсия
+
+#Написать рекурсию, которая содержит i-тое число Фибоначчи
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(7))
+
+print("*****************************************************************")
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
+
