c5fbc1b09501287c522dbd0bbac82363ac61a76f
Dalliya <dalliyal@gmail.com>
2022-08-10 22:22:47 +0200

    Task_m_25_Pythagoras_Table

---
+++ Task_m_25_Pythagoras_Table.py
@@ -0,0 +1,9 @@
+#Напечатать таблицу Пифагора заданной конфигурации.
+
+def print_mult_table(n, m): # prints multiplication table n x m
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            print("{}". format(i, j), end= '\t')
+        print()
+print_mult_table()
+
