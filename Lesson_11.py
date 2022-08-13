7bc69e882c0d19ab63df27c69e1f21515bbc59fb
Dalliya <dalliyal@gmail.com>
2022-08-13 03:23:47 +0200

    Lesson_11

---
+++ Lesson_11.py
@@ -0,0 +1,20 @@
+import string
+import random
+import math
+
+def calc_pi():
+    dots = [[random.random(), random.random()] for i in range(1000)]
+
+    num_of_dots_inside_circle = sum(1 for dot in dots
+                if math.sqrt((0.5-dot[0])**2 + (0.5-dot[1])**2) <= 0.5)
+
+    num_of_dots_inside_square = len(dots)
+
+    my_pi = num_of_dots_inside_circle/(num_of_dots_inside_square*(0.5**2))
+    return my_pi
+
+avr_pi = sum(calc_pi() for _ in range(1000)) / 1000
+print(avr_pi)
+
+
+
