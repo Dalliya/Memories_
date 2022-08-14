6b047b695d5ea3ebf1d61c0517c7ce5961fcb608
Dalliya <dalliyal@gmail.com>
2022-08-14 04:12:02 +0200

    Lesson_11_Lists_Sort_Anonimous_Functions

--- Lesson_11.py
+++ Lesson_11.py
@@ -1,6 +1,8 @@
+import pprint
 import string
 import random
 import math
+import Lesson_8
 
 def calc_pi():
     dots = [[random.random(), random.random()] for i in range(1000)]
@@ -18,7 +20,7 @@ print(avr_pi)
 
 print("*****************************************************************")
 
-#Сортировка списков
+#Сортировка списков. Сортировка списков по ключу
 
 lst = [2, 1, 3]
 lst.sort()
@@ -28,8 +30,100 @@ lst = ['aba', 'b', 'aaa', 'cc']
 lst.sort()
 print(lst)
 
-lst.sort(reverse=True)
+lst.sort(reverse=True, key=len)
 print(lst)
 
+lst.sort(key=len)
+print(lst)
+
+print("*****************************************************************")
+
+def get_distance_from_poi(elem):
+    return abs(10 - elem)
+
+pois = [9, 6, 8, 11, 123, 0]
+
+pois.sort(key=lambda elem: abs(10 - elem))
+print(pois)
+
+#pois.sort(key=get_distance_from_poi)
+#print(pois)
+#pois.sort()
+#print(pois)
+
+print("*****************************************************************")
+
+full_names = [
+    'Михайловский Владимир',
+    'Шишкин Сергей',
+    'Мазур Андрей',
+    'Шияев Павел',
+    'Трусов Дмитрий',
+    'Дулгеров Олег',
+    'Жданова Дария',
+    'Каминская Екатерина',
+    'Железняк Игорь',
+    'Полоз Евгений',
+    'Захидова Сабина',
+    'Лигута Иван',
+    'Величко Алексей'
+]
+
+#def get_first_name(elem):
+#    return elem.split(' ')[1]
+
+#full_names.sort(reverse=True, key=get_first_name)
+#Lesson_8.pretty_print_lst(full_names)
+#pprint.pprint(full_names)
+
+#full_names.sort(reverse=True)
+#pprint.pprint(full_names)
+
+#Анонимные функции(Называются "Лямбды" - это функции в которых есть только тело и логика.
+
+full_names.sort(reverse=True, key=lambda  elem: elem.split(' ')[1])
+Lesson_8.pretty_print_lst(full_names)
+pprint.pprint(full_names)
+
+print("*****************************************************************")
+
+group = [
+    ['Михайловский Владимир', '1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Шишкин Сергей', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Мазур Андрей', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Шияев Павел', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Трусов Дмитрий', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Дулгеров Олег', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Жданова Дария', '1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1'],
+    ['Каминская Екатерина', '0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0'],
+    ['Железняк Игорь', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Полоз Евгений', '1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Захидова Сабина', '1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Лигута Иван', '1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0'],
+    ['Величко Алексей', '1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0']
+]
+
+def print_full_names(group):
+    print('\n\n---Students---')
+    for i in range(len(group)):
+        print(group[i][0])
 
 
+def print_full_names_sorted(group):
+    group.sort()
+    print_full_names(group)
+
+def print_ranks(group):
+    print('\n\n--- Students Ranks ---')
+    for i in range(len(group)):
+        lst = group[i][1].split(',')
+        print(lst)
+
+def print_top_n(students_rank, n):
+    pass
+
+
+
+print_full_names(group)
+print_full_names_sorted(group)
+print_ranks(group)
