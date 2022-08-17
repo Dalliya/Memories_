30ef044b96a2b770f9e1276421f9a6a9a1389961
Dalliya <dalliyal@gmail.com>
2022-08-17 07:20:40 +0200

    Lesson_11_Dictionaries

---
+++ Lesson_11_Dictionaries.py
@@ -0,0 +1,14 @@
+#Dictionaries
+#Сервис укорачивания адресов
+
+import pprint
+
+#initialization
+dict_en_es = {'world' : 'mundo',
+              'language' : 'idioma',
+              'See you later' : 'Hasta la vista'}
+
+print(dict_en_es)
+print(type(dict_en_es))
+
+print(dict_en_es['world'])
