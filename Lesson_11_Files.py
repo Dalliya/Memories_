4884db25e503beafe6376c229eb5b73238ec4ada
Dalliya <dalliyal@gmail.com>
2022-09-25 23:38:52 +0200

    Lesson_12

--- Lesson_11_Files.py
+++ Lesson_11_Files.py
@@ -1,5 +1,6 @@
 import pprint
 import string
+import csv
 
 text = open('hhgttg.txt', encoding='utf8').read().lower() #открыть файл и прочесть его содержимое/ опущение результата в нижний регистр функцией .lower()
 word_list = text.split()#нарезка строки на слова
@@ -20,3 +21,37 @@ for word in word_list: #создание цикла
 
 for word in sorted(word_count, key=word_count.get, reverse=True):
     print('%s -> %d' % (word, word_count[word]))
+
+###########################################################################
+
+def get_data_from_csv(filename):
+
+    f = open(filename, "r")
+    list_dicts = [row for row in csv.DictReader(f)]
+    f.close()
+
+    return list_dicts
+
+def print_age_distribution(passengers):
+#    for passenger in passengers:
+#       print("%s: %s" % (passenger['Name'], passenger['Age']))
+
+#passengers = get_data_from_csv(r'titanic.csv')
+#print_age_distribution(passengers)
+
+    age_distr = {}
+    for person in passengers:
+        age = person['Age']
+        if age:
+            age = float(age)
+            decade = (age // 10) * 10
+            age_distr[decade] = age_distr.get(decade, 0) + 1
+        #else:
+        #   print('Age is missing')
+
+    for k, v in sorted(age_distr.items()):
+        print('%-10s %s' % ('%d [%d]:' % (k, v), v*"\u2015"))
+
+passengers = get_data_from_csv(r'titanic.csv')
+print_age_distribution(passengers)
+
--- Lesson_12_Files.py
+++ Lesson_12_Files.py
@@ -22,7 +22,7 @@ def words_count(path_to_file, path_to_stop_words, top_n = 10):
         #или
         if word not in stop_word_list: #если слово не в списке stop_word_list, то начинаем цикл
             if word: #если слово не пустое
-                word_count[word] = word_count.get(word, 0) + 1 #возврат 0, если значения позаданному ключу word нет
+                word_count[word] = word_count.get(word, 0) + 1 #возврат 0, если значения по заданному ключу word нет
 
 
                 #if word in word_count:#если слово находится в словаре
@@ -34,7 +34,77 @@ def words_count(path_to_file, path_to_stop_words, top_n = 10):
 
     #pprint.pprint(word_count)#Посчитать количество повторений каждого слова в тексте
 
-    for word in sorted(word_count, key=word_count.get, reverse=True)[:top_n]: #sorted()возврат отсортированного списка через функцию/ вычленение подсписка
-        print('%s -> %d' % (word, word_count[word]))
+    for word in sorted(word_count, key=word_count.get, reverse=True)[:top_n]: #sorted()возврат отсортированного списка через функцию/ вычленение подсписка *если мне нужно провести
+        print('%s -> %d' % (word, word_count[word])) #сортировку списка, то просто взять этот фрагмент кода и пользоваться им
 
 words_count(r'C:\Users\utilisateur\PycharmProjects\Memories\hhgttg.txt', r'C:\Users\utilisateur\PycharmProjects\Memories\stop_words_list.txt')
+
+
+print('******************************************************************************************')
+
+# Функция hash()
+
+#hash() - функция, которая преобразует строковое значение в числовое
+#Например: hash('abc')
+##########################################################################################################"
+
+#Modul Dictionaries:
+
+#real examples:
+person_1 = {'name': 'Richard Feynman',
+            'age': 99,
+            'birth_place': 'USA',
+            'birth_date': "1918-01-01",
+            'awards': ('Nobel Prize in Physics', 'USA Science Medal')}
+
+person_2 = {'name': 'Albert Einstein',
+            'age': 138,
+            'birth_place': 'Germany',
+            'birth_date': "1879-03-14",
+            'awards': ('Nobel Prize in Physics', 'Planck Medal')}
+
+person_3 = {'name': 'Nicola Tesla',
+            'age': 161,
+            'birth_place': 'Croatia',
+            'birth_date': "1856-07-10",
+            'awards': ('Edison Medal')}
+
+physicits = (person_1, person_2, person_3)
+pprint.pprint(physicits)
+
+#add new key_value pairs
+person_1['hobby'] = "painting"
+person_2['hobby'] = "violin"
+person_3['hobby'] = "pigeons"
+pprint.pprint(physicits)
+
+#get with default
+employee_1 = {"name": "Alex", "salary": 10000, "dep": "Sales", "bonus": 200}
+employee_2 = {"name": "Nick", "salary": 20000, "dep": "Sales"}
+employee_3 = {"name": "Sue", "salary": 50000, "dep": "IT", "bonus": 500}
+employee_4 = {"name": "Phil", "salary": 5000, "dep": "BoardOfDirectors", "bonus": 100}
+
+employees = [employee_1, employee_2, employee_3, employee_4]
+for i in range(len(employees)):
+    # current_salary = employees[i]['salary']
+    # new_salary = current_salary * 2
+    # employees[i]['salary'] = new_salary
+    employees[i]['salary'] *= 2
+
+pprint.pprint(employees)
+
+for i in range(len(employees)):
+    if 'bonus' in employees[i]:
+        employees[i]['bonus'] *= 2
+    else:
+        employees[i]['bonus'] = 1000
+
+pprint.pprint(employees)
+employees.sort(key=lambda elem: elem['name'])
+pprint.pprint(employees)
+
+employees.sort(key=lambda elem: elem['bonus'])
+pprint.pprint(employees)
+
+
+
